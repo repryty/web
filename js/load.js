a1ed464f1c4a141a9decd407ec31f374e323a3ca
리프리티냥 REPRYTYNANG <62415785+repryty@users.noreply.github.com>
2020-07-31 19:10:30 +0900

    a

--- js/load.js
+++
@@ -1,6 +0,0 @@
-function loadcom(){
-    document.getElementById("loading").style.animationDuration = "2s";
-    setTimeout(function(){
-        document.getElementById("loading").style.top = "-50%";
-    }, 2000)
-}
