3c380e41f5e24b0921ddd611ed5cee99d3278987
리프리티냥 REPRYTYNANG <62415785+repryty@users.noreply.github.com>
2020-10-05 13:35:14 +0900

    a

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,7 @@ function menuclose(){
     document.getElementById("menu").style.left = "-100%";
     document.getElementById("ce1").setAttribute("onClick", "menuopen()");
 }
+function loadcomplete(){
+    setTimeout(function(){document.getElementById("loading").style.opacity = "0%";}, 60);
+    setTimeout(function(){document.getElementById("loading").style.display = "none";}, 600);
+}
